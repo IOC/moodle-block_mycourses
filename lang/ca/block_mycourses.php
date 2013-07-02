2341417f70dc601ab964e4684fba8dc03a78cf4b
Marc Català <reskit@gmail.com>
2013-07-02 12:34:32 +0200

    Added instance lang strings

--- lang/ca/block_mycourses.php
+++ lang/ca/block_mycourses.php
@@ -1,3 +1,5 @@
 <?php
 $string['pluginname'] = 'Els meus cursos';
 $string['mycoursesperpage'] = 'Limita el nombre de cursos que es mostraran';
+$string['mycourses:addinstance'] = 'Afegeix un nou bloc Els meus cursos';
+$string['mycourses:myaddinstance'] = 'Afegeix un nou bloc Els meus cursos a La meva pàgina inicial';
--- lang/en/block_mycourses.php
+++ lang/en/block_mycourses.php
@@ -1,3 +1,5 @@
 <?php
 $string['pluginname'] = 'My courses';
 $string['mycoursesperpage'] = 'Limit the number of courses showing up';
+$string['mycourses:addinstance'] = 'Add a new My courses block';
+$string['mycourses:myaddinstance'] = 'Add a new My courses block to My home';
